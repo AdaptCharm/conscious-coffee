c4da91f46a4c83251f35228b6f198790d26f8b4d
AdaptCharm <jamie@jamie.no>
2020-11-04 08:00:22 +0100

    Removed unused import statements

--- components/core/Layout/Layout.tsx
+++ components/core/Layout/Layout.tsx
@@ -1,12 +1,10 @@
 import { FC } from 'react'
 import { Navbar } from '@components/core'
 import { Container } from '@components/ui'
-import c from 'classnames'
-import l from './Layout.module.css'
 
 const Layout: FC = ({ children }) => {
   return (
-    <div className={c(l.root)}>
+    <div>
       <header>
         <Container>
           <Navbar />
