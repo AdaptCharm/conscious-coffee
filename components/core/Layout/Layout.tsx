c9c1ad5dfbf7a68ce8dae1ac04a06d934c2317c3
AdaptCharm <jamie@jamie.no>
2020-11-07 00:54:42 +0100

    Minor adjustments to the header and its wrapper

--- components/core/Layout/Layout.tsx
+++ components/core/Layout/Layout.tsx
@@ -4,8 +4,8 @@ import { Container } from '@components/ui'
 
 const Layout: FC = ({ children }) => {
   return (
-    <div className="bg-primary mx-auto mw-max h-full ox-none">
-      <header className="fixed z-100">
+    <div className="bg-primary text-secondary h-full ox-none">
+      <header className="fixed g-overlay w-full z-100">
         <Container>
           <Navbar />
         </Container>
