50b04e18d4b7a5fab5b6636ac822db7fdfb28013
AdaptCharm <jamie@jamie.no>
2020-11-06 22:23:53 +0100

    Add max width and fixed positioning to header

--- components/core/Layout/Layout.tsx
+++ components/core/Layout/Layout.tsx
@@ -4,8 +4,8 @@ import { Container } from '@components/ui'
 
 const Layout: FC = ({ children }) => {
   return (
-    <div>
-      <header>
+    <div className="bg-primary mx-auto mw-max h-full ox-none">
+      <header className="fixed z-100">
         <Container>
           <Navbar />
         </Container>
