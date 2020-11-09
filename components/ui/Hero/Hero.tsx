99780f7cd4f3c97b52bec5dd3f8cd6f7f5029735
AdaptCharm <jamie@jamie.no>
2020-11-09 20:10:29 +0100

    Add background, text and button to hero component

--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -6,6 +6,14 @@ const Hero: FC = () => {
   return (
     <section className={h.hero}>
       <div className={h.inner}>
+        <h1 className={h.title}>
+          Think Different, Think Organic
+        </h1>
+        <Link href="/">
+          <a className={h.link} aria-label="Our locations">
+            Our Locations
+          </a>
+        </Link>
       </div>
     </section>
   )
