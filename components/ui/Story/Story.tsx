7e7735c13d22a464783992062f8216ef62cf0c5c
AdaptCharm <jamie@jamie.no>
2020-11-09 22:51:37 +0100

    Add content and styling to story component

--- components/ui/Story/Story.tsx
+++ components/ui/Story/Story.tsx
@@ -3,7 +3,7 @@ import s from './Story.module.css'
 
 const Story: FC = () => {
   return (
-    <section className={s.hero}>
+    <section className={s.story}>
       <div className={s.inner}>
         <div className={s.heading}>
           <h1 className={s.title}>
@@ -12,7 +12,10 @@ const Story: FC = () => {
         </div>
         <div className={s.content}>
           <p className={s.desc}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vivamus arcu felis bibendum ut tristique. Odio eu feugiat pretium nibh ipsum consequat. Magna eget est lorem ipsum dolor sit amet. Tristique senectus et netus et malesuada fames ac turpis egestas. Porta lorem mollis aliquam ut porttitor. Id porta nibh venenatis cras sed. Enim ut sem viverra aliquet eget sit amet tellus. Ut morbi tincidunt augue interdum velit euismod in pellentesque massa. Eleifend donec pretium vulputate sapien nec. Pharetra convallis posuere morbi leo. Malesuada fames ac turpis egestas maecenas pharetra convallis. Malesuada fames ac turpis egestas maecenas. Amet tellus cras adipiscing enim eu turpis egestas.
+            Vivamus arcu felis bibendum ut tristique. Odio eu feugiat pretium nibh ipsum consequat. Magna eget est lorem ipsum dolor sit amet. Tristique senectus et netus et malesuada fames ac turpis egestas.
+          </p>
+          <p className={s.desc}>
+            Porta lorem mollis aliquam ut porttitor. Id porta nibh venenatis cras sed. Enim ut sem viverra aliquet eget sit amet tellus. Ut morbi tincidunt augue interdum velit euismod in pellentesque massa. Eleifend donec pretium vulputate sapien nec. Pharetra convallis posuere morbi leo.
           </p>
         </div>
       </div>
