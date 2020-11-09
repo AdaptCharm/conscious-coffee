27f609335c8b048f29dae7548ee822346ad4f6b5
AdaptCharm <jamie@jamie.no>
2020-11-09 17:44:48 +0100

    Layout refactoring

--- components/core/Layout/Layout.tsx
+++ components/core/Layout/Layout.tsx
@@ -1,17 +1,17 @@
 import { FC } from 'react'
 import { Navbar } from '@components/core'
-import { Container } from '@components/ui'
+import l from './Layout.module.css'
 
 const Layout: FC = ({ children }) => {
   return (
-    <div className="bg-primary text-secondary h-full ox-none">
-      <header className="fixed g-overlay w-full z-100">
-        <Container>
+    <>
+      <header className={l.layout}>
+        <div className={l.inner}>
           <Navbar />
-        </Container>
+        </div>
       </header>
       <main>{children}</main>
-    </div>
+    </>
   )
 }
 
