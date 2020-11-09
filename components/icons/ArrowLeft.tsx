cefab0408da70236b6c06785236ffe9ac77bddf4
AdaptCharm <jamie@jamie.no>
2020-11-09 17:29:55 +0100

    Fix stroke not diplaying properly on arrow icons

--- components/icons/ArrowLeft.tsx
+++ components/icons/ArrowLeft.tsx
@@ -6,7 +6,7 @@ const ArrowLeft = ({ ...props }) => {
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
-      stroke="currentColor"
+      stroke="white"
       { ...props }
     >
       <path d="M19 12H5" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/><path d="M12 19L5 12L12 5" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
--- components/icons/ArrowRight.tsx
+++ components/icons/ArrowRight.tsx
@@ -6,10 +6,10 @@ const ArrowRight = ({ ...props }) => {
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
-      stroke="currentColor"
+      stroke="white"
       { ...props }
     >
-      <path d="M5 12H19" stroke="white" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/><path d="M12 5L19 12L12 19" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M5 12H19" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/><path d="M12 5L19 12L12 19" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
   )
 }
